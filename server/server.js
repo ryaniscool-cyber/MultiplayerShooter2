095ab575f92b653089840c2042d134931b8de39c
ryaniscool-cyber <ryvn.antony@gmail.com>
2025-06-16 14:19:11 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,30 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static("client"));
+
+const players = {};
+
+io.on("connection", socket => {
+  console.log("New player: " + socket.id);
+  players[socket.id] = { x: 0, y: 0, z: 0 };
+
+  socket.on("update", data => {
+    players[socket.id] = data;
+    io.emit("players", players);
+  });
+
+  socket.on("disconnect", () => {
+    delete players[socket.id];
+    io.emit("players", players);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
 
